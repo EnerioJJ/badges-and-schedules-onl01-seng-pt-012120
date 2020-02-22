fc1fd5826bd612d386ad49c923eca4ec63df9329
Jay W <januarydwilson@gmail.com>
2020-02-22 18:49:26 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,26 @@
 # Write your code here.
+
+def badge_maker(name)
+  puts "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  array.each do |name|
+    puts "Hello, my name is #{name}."
+  end
+end
+
+def assign_rooms(num, nam)
+  assignments = []
+  
+  num.each do |name, numbers|
+    puts "Hello, #{name}! You'll be assigned to room #{numbers}!"
+  
+  assignments << [name, numbers]
+  end
+end
+
+def printer
+  batch_badge_creator
+  assign_rooms
+end
