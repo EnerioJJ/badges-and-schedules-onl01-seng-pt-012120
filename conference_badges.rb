f34216034ecd7adf4e60b1b135ef8160e90dcc95
Jay W <januarydwilson@gmail.com>
2020-02-22 18:58:26 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,7 +10,7 @@ end
 
 def assign_rooms(attendees)
   attendees.each_with_index do |people, index|
-    "Hello, #{people}! You'll be assigned to room #{attendees[people]}"
+    "Hello, #{people}! You'll be assigned to room #{attendees[people]}.to_s"
   end
 end
 
